72ddb454004111e4b39a8aa94af76492f236751b
Hannes.Han <hannes.han@grapecity.com>
2023-04-10 14:35:28 +0800

    remove search button

--- src/visual.ts
+++ src/visual.ts
@@ -14,7 +14,7 @@ export default class Visual extends WynVisual {
   private dom : HTMLElement;
 
   private inputEle : HTMLInputElement;
-  private btnEle : HTMLButtonElement;
+  // private btnEle : HTMLButtonElement;
   private host : VisualNS.VisualHost;
   
   private styleOption : any;
@@ -28,6 +28,7 @@ export default class Visual extends WynVisual {
 
   constructor(dom: HTMLDivElement, host: VisualNS.VisualHost, options: VisualNS.IVisualUpdateOptions) {
     super(dom, host, options);
+    Visual.root = this;
     this.dom = dom;
     this.host = host;
     Visual.root = this;
@@ -38,16 +39,23 @@ export default class Visual extends WynVisual {
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
         <input type="search" id="default-search" class="input-ele block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="搜索相关字段..." required>
-        <button type="submit" class="btn-ele text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">搜索</button>
+        
     </div>
     `;
+
+    const btnStr = `<button type="submit" class="btn-ele text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">搜索</button>`;
     this.inputEle = document.getElementsByClassName("input-ele")[0] as HTMLInputElement;
-    this.btnEle = document.getElementsByClassName("btn-ele")[0] as HTMLButtonElement;
+    // this.btnEle = document.getElementsByClassName("btn-ele")[0] as HTMLButtonElement;
 
-    this.btnEle.addEventListener('click',this.Submit);
-    
+    // this.btnEle.addEventListener('click',this.Submit);
+    this.inputEle.addEventListener('blur',this.Submit);
+    this.inputEle.addEventListener('keydown',
+    function(event:KeyboardEvent){
+      if(event.keyCode == 13){
+      Visual.root.Submit(event);
+      }
+  })
   }
-
   private Submit = (e : Event)=>{
     let val = this.inputEle.value;
     if(this.isMock){
@@ -89,7 +97,7 @@ export default class Visual extends WynVisual {
     if(this.styleOption[PLACEHOLDER] != undefined){
       this.inputEle.placeholder = this.styleOption[PLACEHOLDER]||"";
     }
-    if(this.styleOption[BUTTONBGCOLOR]){
+    /* if(this.styleOption[BUTTONBGCOLOR]){
       this.btnEle.style.backgroundColor = this.styleOption[BUTTONBGCOLOR];
     }
     if(this.styleOption[BUTTONTEXT]){
@@ -98,7 +106,7 @@ export default class Visual extends WynVisual {
       this.btnEle.style.fontStyle = this.styleOption[BUTTONTEXT].fontStyle;
       this.btnEle.style.fontWeight = this.styleOption[BUTTONTEXT].fontWeight;
       this.btnEle.style.color = this.styleOption[BUTTONTEXT].color;
-    }
+    } */
     if(this.styleOption[INPUTBGCOLOR]){
       this.inputEle.style.backgroundColor = this.styleOption[INPUTBGCOLOR];
     }
