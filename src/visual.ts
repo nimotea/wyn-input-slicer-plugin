7dd59dd9cc4042cc46bc355adfcd2812309edf5a
Hannes.Han <hannes.han@grapecity.com>
2023-04-25 15:01:41 +0800

    fix text vertical-middle

--- src/visual.ts
+++ src/visual.ts
@@ -47,13 +47,6 @@ export default class Visual extends WynVisual {
     </div>
     `;
 
-    const inputStr = `<div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input type="search" id="default-search" class="input-ele block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="搜索相关字段..." required>
-    </div>`;
-    const btnStr = `<button type="submit" class="btn-ele text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">搜索</button>`;
 
     this.inputEle = document.getElementsByClassName("input-ele")[0] as HTMLInputElement;
     // this.btnEle = document.getElementsByClassName("btn-ele")[0] as HTMLButtonElement;
@@ -156,16 +149,6 @@ export default class Visual extends WynVisual {
     if(this.styleOption[INPUTBOCOLOR]){
       this.inputEle.style.borderColor = this.styleOption[INPUTBOCOLOR];
     }
-    /* if(this.styleOption[BUTTONBGCOLOR]){
-      this.btnEle.style.backgroundColor = this.styleOption[BUTTONBGCOLOR];
-    }
-    if(this.styleOption[BUTTONTEXT]){
-      this.btnEle.style.fontSize = this.styleOption[BUTTONTEXT].fontSize;
-      this.btnEle.style.fontFamily = this.styleOption[BUTTONTEXT].fontFamily;
-      this.btnEle.style.fontStyle = this.styleOption[BUTTONTEXT].fontStyle;
-      this.btnEle.style.fontWeight = this.styleOption[BUTTONTEXT].fontWeight;
-      this.btnEle.style.color = this.styleOption[BUTTONTEXT].color;
-    } */
     if(this.styleOption[INPUTBGCOLOR]){
       this.inputEle.style.backgroundColor = this.styleOption[INPUTBGCOLOR];
     }
